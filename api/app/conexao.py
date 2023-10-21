17145dddfda7c1dac0a82e2f19728dd10448c12b
CeciliaMarques <cecilia.marques.m.m@gmail.com>
2023-10-20 23:36:10 -0300

    Criando  tabela, criando usuario, atualizando e deletando

--- api/app/conexao.py
+++ api/app/conexao.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+from werkzeug.security import generate_password_hash
 import psycopg2
 import sys
 
@@ -22,3 +23,44 @@ else:
       print ("Conectado!")
 
       cursor = con.cursor() 
+   
+
+def criar_tabelas():
+      
+      cursor.execute("CREATE TABLE IF NOT EXISTS usuarios (id_usuario serial PRIMARY KEY, nome VARCHAR(50), email VARCHAR(50),"
+                     "telefone VARCHAR,  cpf VARCHAR(14), nivel INTEGER, senha VARCHAR(200));")
+
+    # Commit (salvar) as mudanças
+      con.commit()
+      return jsonify("Criei as tabelas") 
+def inserir_usuario(dados): 
+
+      cursor.execute("INSERT INTO usuarios (nome, email, telefone, cpf, nivel, senha ) VALUES (%s, %s, %s, %s, %s, %s);",
+                        (dados["nome"],  dados["email"], dados["telefone"], dados["cpf"],dados["nivel"], generate_password_hash(dados["senha"]) ))
+      con.commit()
+      return jsonify({'message': 'Cadastrado com sucesso.'}), 201
+
+def atualizar_usuario(dados):
+      cursor.execute("UPDATE usuarios SET nome = %s, email = %s, telefone = %s, cpf =%s, nivel = %s, senha = %s WHERE id_usuario =%s",
+                              (dados["nome"], dados["email"], dados["telefone"], dados["cpf"], dados["nivel"],generate_password_hash(dados["senha"]), dados["id_usuario"]))
+      con.commit()
+      return jsonify({'message': 'Atualizado com sucesso.', 'data': dados}), 200 
+
+def deletar_usuario(id_usuario):
+    try:
+        cursor.execute("DELETE FROM usuarios WHERE id_usuario = %s;", (id_usuario,))
+        con.commit()
+
+        return jsonify({'message': 'Deletado com sucesso.'}), 200
+    
+    except Exception as e:
+        return jsonify({'message': 'Erro ao excluir o usuário.', 'error': str(e)}), 500
+    finally:
+            if con: 
+                  #con.close()
+                  pass
+                  
+
+
+def fechar_conexao():
+      con.close()
--- api/app/rotas.py
+++ api/app/rotas.py
@@ -7,3 +7,29 @@ from app import conexao
 @app.route("/")
 def index():
     return "Olá Mundo!!!"
+
+@app.route("/criar/tabelas")
+def criar_tabelas():
+    con = conexao
+    resposta= con.criar_tabelas()
+    return resposta
+
+@app.route("/inserir/usuario",  methods=['POST'])
+def inserir_usuario():    
+    con  = conexao
+    dados = request.get_json(force=True)
+    resposta = con.inserir_usuario(dados)
+    return resposta
+
+@app.route('/atualizar/usuario',methods=['PUT'])
+def atualizar_usuario():    
+    con  = conexao
+    dados = request.get_json(force=True)
+    resposta = con.atualizar_usuario(dados)
+    return resposta
+
+@app.route('/deletar/usuario/<id_usuario>',methods=['DELETE'])
+def deletar_usuario(id_usuario):    
+    con  = conexao
+    resposta = con.deletar_usuario(id_usuario)
+    return resposta
