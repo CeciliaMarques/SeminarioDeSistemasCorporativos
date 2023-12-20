9c75da242703eedc0f066b5fb26a44ac39fdf27c
CeciliaMarques <cecilia.marques.m.m@gmail.com>
2023-12-20 11:37:23 -0300

    Moficando porta

--- micro_usuario/micro.py
+++ micro_usuario/micro.py
@@ -3,7 +3,7 @@ from app import app
 
 if __name__ =='main':
 
-    porta = int(os.getenv('PORT'), '3000')
+    porta = int(os.getenv('PORT'), '8000')
     app.run(host='0.0.0.0', port=porta)
 
     #Pega a porta que o servidor está rodando se não tiver nenhuma setada use a porta 5000
